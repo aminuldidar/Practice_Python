e522dbc7319280131f2e354216b9083cc7ea4842
Md. Aminul Islam <aminul.didar@gmail.com>
2021-07-20 13:54:23 +0600

    Delete function_list.py

--- function_list.py
+++
@@ -1,8 +0,0 @@
-def fun(a, b, c, *x):
-    print('This a: %s' % a)
-    print('This b: %s' % b)
-    print('This c: %s' % c)
-    y=list(x)
-    print('This x: %s' % y)
-    
-fun('aa', 'bb', 'cc', 'p', 'q', 'r')
